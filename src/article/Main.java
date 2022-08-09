9b2907a4d09354ca07cfc22bd0b96853b91b54db
its-jys <jys20070806@gmail.com>
2022-08-09 15:15:04 +0900

    스캐너 연습

---
+++ src/article/Main.java
@@ -0,0 +1,13 @@
+package article;
+ 
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("ㅎㅇ");
+		Scanner 스캐너 = new Scanner(System.in);
+		int cmd = 스캐너.nextInt();
+		스캐너.close();
+		System.out.println(cmd);
+	}
+}
